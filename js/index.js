8fd9103650989d406ab8e325c3e8411ec40cfe91
Andrii-Vekker <65786577+Andrii-Vekker@users.noreply.github.com>
2022-06-30 10:45:30 +0300

    уцек

---
+++ js/index.js
@@ -0,0 +1,55 @@
+// console.log(document);
+
+// const body = document.body;
+// console.log(body);
+
+// const list = body.firstElementChild;
+// console.log(list);
+
+// const firstListItem = list.firstElementChild;
+// console.log(firstListItem);
+
+// const listItems = list.children;
+// console.log(listItems);
+// const listWithId = document.querySelector('#menu');
+// listWithId.style.textTransform = 'uppercase';
+// listWithId.style.fontSize = '24px';
+// listWithId.style.listStyle = "none"
+// console.log(listWithId);
+
+// const listWithClass = document.querySelector('.menu');
+// listWithClass.style.color = "green"
+// console.log(listWithClass);
+
+// const menuItemsByTagName = document.querySelectorAll("li");
+// console.log(menuItemsByTagName);
+
+// const menuItemsByClass = document.querySelectorAll(".menu-item");
+// console.log(menuItemsByClass);
+
+// const firstMenuItem = document.querySelector(".menu-item");
+// firstMenuItem.style.color = 'tomato';
+// console.log(firstMenuItem);
+
+
+//===================================================
+// const message = document.querySelector("#message");
+// console.log(message.value); // Default textarea message
+
+// console.log(div)
+// //===================================================
+// const activeLink = document.querySelector(".btn.active");
+// console.log(activeLink.href); // https://s.codepen.io/about
+
+// //===================================================
+// const image = document.querySelector(".image");
+// image.src.style.textDecoration = "none"
+// console.log(image.src); // https://placeimg.com/640/480/animals
+// image.src = 'https://placeimg.com/640/480/tech';
+
+
+const text = document.querySelector(".article-text");
+console.log(text.textContent); // text inside p.article-text
+
+const title = document.querySelector(".article-title");
+title.textContent = 'Welcome to Bahamas! PETUH';
